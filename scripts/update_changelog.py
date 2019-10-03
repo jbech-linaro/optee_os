85ce865724197081a5dcaa5354befe81ca3dfd1e
Joakim Bech <joakim.bech@linaro.org>
2019-10-03 14:42:36 +0200

    fixup! scripts: add update_changelog.py

--- scripts/update_changelog.py
+++ scripts/update_changelog.py
@@ -4,6 +4,7 @@
 #
 # SPDX-License-Identifier: BSD-2-Clause
 
+from subprocess import Popen, PIPE
 import argparse
 
 
@@ -15,18 +16,14 @@ def get_args():
                                      ' --changelog-file CHANGELOG.md'
                                      ' --release-version 3.7.0'
                                      ' --previous-release-version 3.6.0'
-                                     ' --release-date 2019-10-11'
-                                     ' --previous-release-date 2019-07-05')
+                                     ' --release-date 2019-10-11')
 
-    parser.add_argument('--changelog-file', action='store', required=True,
+    parser.add_argument('--changelog-file', action='store', required=False,
+                        default='CHANGELOG.md',
                         help='Changelog file to be updated.')
 
     parser.add_argument('--release-date', action='store', required=True,
                         help='The release date (yyyy-mm-dd).')
-
-    parser.add_argument('--previous-release-date', action='store',
-                        required=True,
-                        help='The previous release date (yyyy-mm-dd).')
 
     parser.add_argument('--release-version', action='store', required=True,
                         help='Release version (MAJOR.MINOR.PATCH).')
@@ -46,44 +43,51 @@ def prepend_write(filename, text):
         f.flush()
 
 
+def get_previous_release_date(tag):
+    cmd = "git log -1 --date=format:%Y-%m-%d --format=format:%cd " \
+          "{}".format(tag)
+    process = Popen(cmd.split(), stdout=PIPE)
+    (output, err) = process.communicate()
+    return output.decode("utf-8")
+
+
 def main():
     global args
 
     args = get_args()
 
+    gits = ["OP-TEE/optee_os", "OP-TEE/optee_client", "OP-TEE/optee_test",
+            "OP-TEE/build", "linaro-swg/optee_examples"]
+
     # Shorten name
     clf = args.changelog_file
-    rd = args.release_date
-    prd = args.previous_release_date
     rv = args.release_version
     prv = args.previous_release_version
+    rd = args.release_date
+    prd = get_previous_release_date(prv)
 
     # In some cases we need underscore in string
     rvu = rv.replace('.', '_')
 
     text = "# OP-TEE - version {} ({})\n".format(rv, rd)
     text += "\n"
-    text += "- Link to the GitHub [release page][github_release_{}] " \
-            "(optee_os)\n".format(rvu)
-    text += "- Links to the commits and pull requests merged into this " \
-            "release for:\n"
-
-    gits = ["OP-TEE/optee_os", "OP-TEE/optee_client", "OP-TEE/optee_test",
-            "OP-TEE/build", "linaro-swg/optee_examples"]
+    text += "- Links to the release pages, commits and pull requests merged " \
+            "into this release for:\n"
 
     for g in gits:
         gu = g.replace('/', '_')
         gu = gu.replace('-', '_')
-        text += "  - {}: [commits][{}_commits_{}] and [pull requests]" \
-                "[{}_pr_{}]\n".format(g, gu, rvu, gu, rvu)
+        text += "  - {}: [release page][{}_release_{}], " \
+                "[commits][{}_commits_{}] and [pull requests]" \
+                "[{}_pr_{}]\n".format(g, gu, rvu, gu, rvu, gu, rvu)
 
     text += "\n"
-    text += "[github_release_{}]: https://github.com/OP-TEE/optee_os/" \
-            "releases/tag/{}\n".format(rvu, rv)
 
     for g in gits:
         gu = g.replace('/', '_')
         gu = gu.replace('-', '_')
+        text += "\n[{}_release_{}]: https://github.com/{}/releases/tag/" \
+                "{}\n".format(gu, rvu, g, rv)
         text += "[{}_commits_{}]: https://github.com/{}/compare/" \
                 "{}...{}\n".format(gu, rvu, g, prv, rv)
         text += "[{}_pr_{}]: https://github.com/{}/pulls?q=is%3Apr+is%3A" \
